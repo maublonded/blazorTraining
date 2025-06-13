8452521909c1dd3b3a63609c78116f857fe51b5f
maublonded <mauricioht13@gmail.com>
2025-06-12 19:32:26 -0700

    parts practiced today:
    -components
    -EditForms
    -submit buttons
    -object creation

---
+++ blazorTrainingDay1/Models/UsersModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace blazorTrainingDay1.Models
+{
+	public class UsersModel
+	{
+		[Required(ErrorMessage = "User name is required")]
+		public string Name { get; set; }
+		[Range(18, 110, ErrorMessage = "Age must be between 18 and 110")]
+		public int Age { get; set; }
+		[Required(ErrorMessage = "Email is required")]
+		[EmailAddress(ErrorMessage = "E-Mail adress not valid")]
+		public string Email { get; set; }
+		[Required, MinLength(6, ErrorMessage = "6 Characters minimum")]
+		public string Password { get; set; }
+	}
+}
